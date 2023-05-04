69895cfeb1fb5ed132c70c2a1b10e4d329181249
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-05-04 17:28:01 +0200

    feat: adding create file & read input

---
+++ 02-write-file/index.js
@@ -0,0 +1,26 @@
+const readline = require('readline');
+const fs = require('fs');
+
+const fileStream = fs.createWriteStream('text.txt');
+
+// Выводим приветственное сообщение
+console.log('Введите текст или "exit" для выхода:');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', (input) => {
+
+    if (input.toLowerCase() === 'exit') {
+
+        fileStream.end();
+        console.log('Спасибо за информацию!');
+        process.exit();
+    } else {
+
+        fileStream.write(input + '\n');
+    }
+});
+
